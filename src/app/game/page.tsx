5cc54192875bf09d6bec652bce7cf3c3f2883783
Nooqwaffle <jdonutiam@gmail.com>
2023-10-19 15:57:38 -0400

    API integration

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { useEffect, useState } from 'react';
-import {useAsync} from 'react-async';
 import Modal from 'react-modal';
 import Countdown from 'react-countdown';
 import { Question, getCategories, getQuestions } from './questions';
@@ -27,6 +26,14 @@ function Answer({choice, onChosen}) {
     )
 }
 
+function GameCompleted() {
+    return <div className="flex h-screen flex-col items-center">
+         <div className="animate-wiggle delay-100 m-auto bg-stone-100 border-stone-400 border-2 w-4/6 p-10 flex items-center justify-center rounded-3xl">
+            <p className="text-stone-600 text-4xl text-center">All done!! Good game!</p>
+        </div>
+    </div>
+}
+
 function Game({questions}) {
     const [players, setPlayers] = useState<Array<Player>>([
         {name: "Julie", score: 0, answeredThisRound: false},
@@ -34,12 +41,16 @@ function Game({questions}) {
         {name: "Frank", score: 0, answeredThisRound: false}
     ]);
     const [modalIsOpen, setIsOpen] = useState(false);
+    const [questionIndex, setQuestionIndex] = useState(0);
+    const [roundTimer, setRoundTimer] = useState(Date.now() + 60000);
+    const [roundCompleted, setRoundCompleted] = useState(false);
     const [currentQuestion, setCurrentQuestion] = useState<Question>(questions[0]);
+    const [gameCompleted, setGameCompleted] = useState(false);
     const [selectedAnswer, setSelectedAnswer] = useState();
 
     useEffect(() => {
         setCurrentQuestion(questions[0]);
-    }, [questions])
+    }, [])
 
     function openModal() {
         setIsOpen(true);
@@ -52,8 +63,10 @@ function Game({questions}) {
         setIsOpen(false);
     }
     function onChosen(choice: string) {
-        setSelectedAnswer(choice);
-        openModal();
+        if (!roundCompleted) {
+            setSelectedAnswer(choice);
+            openModal();
+        }
     }
 
     function increaseScore(p_name: string) {
@@ -66,10 +79,26 @@ function Game({questions}) {
             return p;
         });
         setPlayers(nextPlayers);
-        console.log(selectedAnswer);
-        console.log(currentQuestion.correct_answer);
-        console.log(nextPlayers);
+
+        if (nextPlayers.find(p => p.answeredThisRound == false) === undefined) {
+            // everyone has answered
+            setRoundTimer(Date.now() + 1);
+            setTimeout(resetRound, 5000);
+        }
         closeModal();
+    }
+
+    function resetRound() {
+        if (questionIndex + 1 !== questions.length) {
+            console.log("Resetting round...")
+            setQuestionIndex(questionIndex + 1);
+            setCurrentQuestion(questions[questionIndex + 1]);
+            setRoundTimer(Date.now() + 60000);
+            setRoundCompleted(false);
+        } else {
+            setGameCompleted(true);
+        }
+        
     }
 
     const customStyles = {
@@ -82,8 +111,12 @@ function Game({questions}) {
           transform: 'translate(-50%, -50%)',
         },
       };
+      
+    function onComplete() {
+        setTimeout(resetRound, 5000);
+    }
 
-    return (
+    return gameCompleted ? <GameCompleted></GameCompleted> : (
         <div className="flex h-screen flex-col items-center">
             <Modal
                 isOpen={modalIsOpen}
@@ -102,7 +135,9 @@ function Game({questions}) {
             </Modal>
             <QuestionElement text={currentQuestion.question}></QuestionElement>
             <div className="m-auto w-3/6">
-                <div className='m-auto text-center'><Countdown className="text-3xl text-stone-600" date={Date.now() + 60000} key='countdown'></Countdown></div>
+                <div className='m-auto text-center'><Countdown className="text-3xl text-stone-600" date={roundTimer} onComplete={onComplete} key={roundTimer}>
+                     <p>The correct answer is <strong>{currentQuestion.correct_answer}</strong>!</p>
+                </Countdown></div>
                 
                 <div className="grid grid-cols-2 grid-rows-2">
                     {
@@ -115,7 +150,6 @@ function Game({questions}) {
 }
 
 export default function Wrapper() {
-
     const [questions, setQuestions] = useState();
     const [loading, setLoading] = useState(true);
 
