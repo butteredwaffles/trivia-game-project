9b237c577d1303dd7314282de2cf7bb572e27484
Nooqwaffle <jdonutiam@gmail.com>
2023-10-19 14:07:26 -0400

    integrate questions with game

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,10 +1,17 @@
 'use client'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import {useAsync} from 'react-async';
 import Modal from 'react-modal';
 import Countdown from 'react-countdown';
-import { getCategories, getQuestions } from './questions';
+import { Question, getCategories, getQuestions } from './questions';
 
-export function Question({text}) {
+export type Player = {
+    name: string,
+    score: number,
+    answeredThisRound: boolean;
+}
+
+function QuestionElement({text}) {
     return (
         <div className="animate-wiggle delay-100 m-auto bg-stone-100 border-stone-400 border-2 w-4/6 p-10 flex items-center justify-center rounded-3xl">
             <p className="text-stone-600 text-3xl text-center">{text}</p>
@@ -12,7 +19,7 @@ export function Question({text}) {
     )
 }
 
-export function Answer({choice, onChosen}) {
+function Answer({choice, onChosen}) {
     return (
         <button className="bg-stone-100 border-stone-400 border-2 m-5 rounded-3xl transition hover:scale-110 duration-300" onClick={() => onChosen(choice)}>
             <p className="text-stone-600 text-3xl p-5 text-center m-auto" >{choice}</p>
@@ -20,12 +27,19 @@ export function Answer({choice, onChosen}) {
     )
 }
 
-export default function Game() {
-    const [choices, setChoices] = useState({});
+function Game({questions}) {
+    const [players, setPlayers] = useState<Array<Player>>([
+        {name: "Julie", score: 0, answeredThisRound: false},
+        {name: "Max", score: 0, answeredThisRound: false},
+        {name: "Frank", score: 0, answeredThisRound: false}
+    ]);
     const [modalIsOpen, setIsOpen] = useState(false);
-    const players = ['Julie', 'John', 'Mary'];
-    getCategories().then((res) => console.log(res));
-    getQuestions().then((res) => console.log(res));
+    const [currentQuestion, setCurrentQuestion] = useState<Question>(questions[0]);
+    const [selectedAnswer, setSelectedAnswer] = useState();
+
+    useEffect(() => {
+        setCurrentQuestion(questions[0]);
+    }, [questions])
 
     function openModal() {
         setIsOpen(true);
@@ -38,8 +52,26 @@ export default function Game() {
         setIsOpen(false);
     }
     function onChosen(choice: string) {
+        setSelectedAnswer(choice);
         openModal();
     }
+
+    function increaseScore(p_name: string) {
+        let nextPlayers = players.map((p, i) => {
+            if (p_name === p.name) {
+                if (currentQuestion.correct_answer === selectedAnswer) p.score++;
+                p.answeredThisRound = true;
+            }
+            
+            return p;
+        });
+        setPlayers(nextPlayers);
+        console.log(selectedAnswer);
+        console.log(currentQuestion.correct_answer);
+        console.log(nextPlayers);
+        closeModal();
+    }
+
     const customStyles = {
         content: {
           top: '50%',
@@ -52,7 +84,7 @@ export default function Game() {
       };
 
     return (
-        <main className="flex h-screen flex-col items-center">
+        <div className="flex h-screen flex-col items-center">
             <Modal
                 isOpen={modalIsOpen}
                 onAfterOpen={afterOpenModal}
@@ -60,22 +92,46 @@ export default function Game() {
                 style={customStyles}
                 contentLabel="Player"
             >
-                {players.map(function(object, i){
-                    return <><input type='radio' name='players' id={object} value={object} key={i} /> {object}<br></br></>;
-                })}
+               <div className="grid grid-cols-2 grid-rows-2">
+                    {
+                        players.map((p) => {
+                            if (!p.answeredThisRound) return <Answer key={p.name} choice={p.name} onChosen={increaseScore}></Answer>
+                        })
+                    }
+               </div>
             </Modal>
-            <Question text="What year was the Declaration of Independence signed?"></Question>
+            <QuestionElement text={currentQuestion.question}></QuestionElement>
             <div className="m-auto w-3/6">
                 <div className='m-auto text-center'><Countdown className="text-3xl text-stone-600" date={Date.now() + 60000} key='countdown'></Countdown></div>
                 
                 <div className="grid grid-cols-2 grid-rows-2">
-                    <Answer choice="1776" onChosen={onChosen}></Answer>
-                    <Answer choice="1876" onChosen={onChosen}></Answer>
-                    <Answer choice="1998" onChosen={onChosen}></Answer>
-                    <Answer choice="2013" onChosen={onChosen}></Answer>
+                    {
+                        currentQuestion.combined_answers.map(a => <Answer key={a} choice={a} onChosen={onChosen}></Answer>)
+                    }
                 </div>
             </div>
-            
-        </main>
+        </div>
     );
+}
+
+export default function Wrapper() {
+
+    const [questions, setQuestions] = useState();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchData() {
+            let raw = await getQuestions(10, "multiple");
+            setQuestions(raw);
+            setLoading(false);
+        }
+
+        fetchData();
+    }, [])
+
+    return (
+        
+           loading ? (<p>Loading...</p>) : (<Game questions={questions}></Game>)
+        
+    )
 }
--- src/app/game/questions.tsx
+++ src/app/game/questions.tsx
@@ -1,13 +1,14 @@
 'use client'
+var he = require('he');
 
-export type Question = {
-    category: string,
-    type: string,
-    difficulty: string,
-    question: string,
-    correct_answer: string,
-    incorrect_answers: Array<string>
-}
+// export type Question = {
+//     category: string,
+//     type: string,
+//     difficulty: string,
+//     question: string,
+//     correct_answer: string,
+//     incorrect_answers: Array<string>
+// }
 
 export type Category = {
     id: number,
@@ -19,11 +20,33 @@ export async function getCategories() {
     return categories;
 }
 
+export class Question {
+    category: string
+    type: string
+    difficulty: string
+    question: string
+    correct_answer: string
+    incorrect_answers: Array<string>
+    combined_answers: Array<string>
+
+    constructor(obj) {
+        this.category = obj.category;
+        this.type = obj.type;
+        this.difficulty = obj.difficulty;
+        this.correct_answer = obj.correct_answer;
+        this.question = he.decode(obj.question);
+        this.incorrect_answers = obj.incorrect_answers.map(a => he.decode(a));
+        this.combined_answers = this.incorrect_answers.concat(this.correct_answer).map(a => he.decode(a)).sort();
+    }
+}
+
 
 export async function getQuestions(quantity: number = 10, type: string = "multiple", difficulty: string = "easy", category: number = -1) {
     let questions: Array<Question> = [];
     let cat = category == -1 ? "" : `category={category}`;
-    console.log(`https://opentdb.com/api.php?amount=${quantity}&difficulty=${difficulty}&type=${type}&${cat}`);
-    questions = await fetch(`https://opentdb.com/api.php?amount=${quantity}&difficulty=${difficulty}&type=${type}&${cat}`).then((response) => response.json()).then((data) => data['results']);
+    let resp = await fetch(`https://opentdb.com/api.php?amount=${quantity}&difficulty=${difficulty}&type=${type}&${cat}`);
+    let json = await resp.json();
+    questions = json["results"].map((r: object) => new Question(r));
+
     return questions;
 }
